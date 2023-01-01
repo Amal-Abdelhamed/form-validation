67c6e59c0ccbf660110bd1cc0b73dba54c736aa1
amal-abdelhameed <amal@atentec.com>
2023-01-01 14:49:14 +0200

    anhance code

--- js/main.js
+++ js/main.js
@@ -12,60 +12,64 @@ let email = document.getElementById('email')
 let submit = document.getElementById('submit')
 
 
-// form.addEventListener('submit', e => {
-//     e.preventDefault();
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-//     validateInputs();
-// });
+    validateInputs();
+});
 function validateInputs() {
-    if (v1 == v2 ==v3 ==v4 ==v5 ==v6 ==v7==v8== false ) {
-        return false;
-    }else{
+    if (vFname() && vLname () && vAge() && vPass() && checkCountry () &&  vPass()) {
         alert('your application submitted successfully')
+        var emptyInputs = document.getElementsByTagName("input")
+        for (var i of emptyInputs) {
+            i.value='';
+        }
+        passError.classList.add('d-none')
+
     }
 }
 // **************first name validation ***********
-var v1 = fName.onkeyup = function () {
+var vFname = fName.onkeyup = function () {
 
     var y = fName.value.length
     if (y < 2) {
         fNameError.classList.remove('d-none')
         lName.setAttribute('disabled', '')
-        return v1 = false;
+        return false;
 
     } else {
         lName.removeAttribute('disabled')
         fNameError.classList.add('d-none')
-        return v1 = true;
+        return true;
     }
 }
 
 // **************last name validation ***********
-var v2 = lName.onkeyup = function () {
+var vLname = lName.onkeyup = function () {
 
     var y = lName.value.length
     if (y < 2) {
         lNameError.classList.remove('d-none')
-        return v2 = false;
+        return false;
     } else {
         lNameError.classList.add('d-none')
-        return v2 = true;
+        return true;
     }
 }
 // ************** age validation ***********
-var v3 = age.onkeyup = function () {
+var vAge = age.onkeyup = function () {
 
     var y = age.value
     if (y <= 18) {
         ageError.classList.remove('d-none')
-        return v3 = false;
+        return false;
     } else {
         ageError.classList.add('d-none')
-        return v3 = true;
+        return true;
     }
 }
 // **************password validation ************
-var v4 = pass.onkeyup = function () {
+var vPass = pass.onkeyup = function () {
 
     var y = pass.value.length
     if (y <= 6) {
@@ -74,7 +78,7 @@ var v4 = pass.onkeyup = function () {
         passError.classList.remove('text-success')
         passError.classList.remove('bg-light')
         passError.classList.remove('text-secondary')
-        return v4 = false;
+        return  false;
 
 
     } else if (y > 6 && y < 10) {
@@ -83,13 +87,13 @@ var v4 = pass.onkeyup = function () {
         passError.classList.add('text-secondary')
         passError.classList.remove('text-success')
         passError.classList.add('bg-light')
-        return v4 = false;
+        return false;
     } else if (y >= 10) {
         passError.textContent = 'your password is strong'
         passError.classList.remove('d-none')
         passError.classList.add('text-success')
         passError.classList.add('bg-light')
-        return v4 = true;
+        return true;
     }
 }
 var show = function () {
@@ -103,20 +107,20 @@ var show = function () {
     }
 }
 // **************message validation ***********
-var v5 = message.onkeyup = function () {
+message.onkeyup = function () {
 
     var y = message.value.length
     if (y < 20) {
         msgError.classList.remove('d-none')
-        return v5 = false;
+        return false;
     } else {
         msgError.classList.add('d-none')
-        return v5 = true;
+        return true;
     }
 }
 // **************file validation ***********
 
-var v6 = checkFiles = function () {
+ function checkFiles() {
     var fullPath = file.value;
     var fileName = fullPath.substring(fullPath.lastIndexOf('\\') + 1);
     var ext = fullPath.substring(fullPath.lastIndexOf('.') + 1);
@@ -125,13 +129,13 @@ var v6 = checkFiles = function () {
         fileError.classList.remove('d-none')
         fileError.classList.remove('text-success')
         fileError.classList.remove('bg-light')
-        return v6 = false;
+        return false;
     } else {
         fileError.textContent = fileName;
         fileError.classList.remove('d-none')
         fileError.classList.add('text-success')
         fileError.classList.add('bg-light')
-        return v6 = true;
+        return true;
     }
 
 }
@@ -139,27 +143,27 @@ var v6 = checkFiles = function () {
 
 
 // **************country validation ***********
-var v7 = checkCountry = function () {
+function checkCountry  ()  {
     if (document.myForm.country.value == '-1') {
         countryError.classList.remove('d-none')
-        return v7 = false;
+        return false;
     } else {
         countryError.classList.add('d-none')
-        return v7 = true
+        return true
     }
 }
 
 // **************email validation ***********
-var v8 = email.onkeyup = function vPass() {
+email.onkeyup = function vPass() {
     var x = email.value;
     var atindex = x.indexOf("@");
     var dotindex = x.lastIndexOf(".");
     if (atindex < 4 || dotindex - atindex < 3 || x.length - dotindex < 3) {
         emailError.classList.remove('d-none')
-        return v8 = false;
+        return false;
     } else {
         emailError.classList.add('d-none')
-        return v8 = true;
+        return true;
     }
 }
 
@@ -171,7 +175,7 @@ function clearErrorMsgs() {
         i.classList.add('d-none')
     }
 }
-function emptyMsg(){
+function emptyMsg() {
     message.value = ''
 
 }
